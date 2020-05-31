fbb76659beba491f3c1eda62321ba9372958bef0
Ubuntu <dllanos@vmBackupUbuntudllanos.0fmieek403futljchiyqoth2sa.bx.internal.cloudapp.net>
2020-05-31 03:30:22 +0000

    commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
